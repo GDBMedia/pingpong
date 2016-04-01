a6930cb23295c6d464a8dd07a70381c3db8fa444
Garrett Biernat <garrett@gdbmedia.net>
2016-04-01 08:28:48 -0700

    business logic
    finished business logic and added error checking

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,36 @@
 function pingpong (num){
-
+ var arrayOfNums = [];
+ var z = 1
+ for(var i = 0; z < num+1; i++){
+ 	if(z%15 === 0){
+ 		arrayOfNums[i] = "PingPong";
+ 	}
+ 	else if(z%5 === 0){
+ 		arrayOfNums[i] = "Pong";
+ 	}
+ 	else if(z%3 === 0){
+ 		arrayOfNums[i] = "Ping";
+ 	}
+ 	else{
+ 		arrayOfNums[i] = z;
+ 	}
+ 	$("#resultT").append("<li>"+arrayOfNums[i]+"</li>");
+ 	z++
+ }
 
 	
 }
-
-
-
-
 $(function(event){
 	$("#submit").click(function(event){
 		event.preventDefault();
 		var number = parseFloat($("#number").val());
-
+			$("#resultT").text("");
 		if(Number.isInteger(number)){
 			var result = pingpong(number);
 			$("#result").show();
-			$("#resultT").text(result);
-
 		}else{
-			alert("you're an idiot");
-		}
-		
-
-		
+			$("#group").addClass("has-error");
+		}	
 	});
 });
 
